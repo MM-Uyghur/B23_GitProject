496a715d05c418c9fee19f83d95301d3f0a1c5de
MM-Uyghur <mm-well@outlook.com>
2021-07-19 16:44:42 -0400

    i am done with this

--- src/gitCOOL.java
+++ src/gitCOOL.java
@@ -1,6 +1,14 @@
 public class gitCOOL {
     public static void main(String[] args) {
         System.out.println("coming to the end");
+        // we have added git-hub account under intellij
+        // make sure to click apply and to save
+        // we shared project under git-hub account
+        // share project on git-hub only one time
+        //it combine the tree action blow
+        // create remote repository under your account
+        // create link between repo to commmmmmmm
+        // push local comits to remote repo
 
     }
 }
